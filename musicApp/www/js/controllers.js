50482a124e4614442d071cc9aef363df5ed861b7
Devin Bjelland <dbjellan@macalester.edu>
2016-03-01 17:22:55 -0600

    Add snap svg

--- musicApp/www/js/controllers.js
+++ musicApp/www/js/controllers.js
@@ -1,6 +1,19 @@
 angular.module('starter.controllers', [])
 
 .controller('InstrumentCtrl', function($scope) {
+  var s = Snap("#keyboard");
+  // Lets create big circle in the middle:
+  var bigCircle = s.circle(150, 150, 100);
+  // By default its black, lets change its attributes
+  bigCircle.attr({
+      fill: "#bada55",
+      stroke: "#000",
+      strokeWidth: 5
+  });
+  // Now lets create another small circle:
+  var smallCircle = s.circle(100, 150, 70);
+  // Lets put this small circle and another one into a group:
+  var discs = s.group(smallCircle, s.circle(200, 150, 70));
 
 })
 
