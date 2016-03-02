1c81d6f29321c99d883003fac26b902b5b139f5a
Devin Bjelland <dbjellan@macalester.edu>
2016-03-02 14:32:04 -0600

    included lib

--- musicApp/www/js/controllers.js
+++ musicApp/www/js/controllers.js
@@ -3,17 +3,17 @@ angular.module('starter.controllers', [])
 .controller('InstrumentCtrl', function($scope) {
   var s = Snap("#keyboard");
   // Lets create big circle in the middle:
-  var bigCircle = s.circle(150, 150, 100);
+  var key = s.rect(30, 50, 30, 100);
   // By default its black, lets change its attributes
-  bigCircle.attr({
-      fill: "#bada55",
+  key.attr({
+      fill: "white",
       stroke: "#000",
       strokeWidth: 5
   });
   // Now lets create another small circle:
-  var smallCircle = s.circle(100, 150, 70);
+  //var smallCircle = s.circle(100, 150, 70);
   // Lets put this small circle and another one into a group:
-  var discs = s.group(smallCircle, s.circle(200, 150, 70));
+  //var discs = s.group(smallCircle, s.circle(200, 150, 70));
 
 })
 
