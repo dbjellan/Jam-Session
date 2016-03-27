a97c05a9c3a2130b633939e555d39400782a311e
awetzel1 <awetzel1@macalester.edu>
2016-03-27 15:34:43 -0500

    doing this to let me merge, nothing really added

--- musicApp/www/js/app.js
+++ musicApp/www/js/app.js
@@ -50,4 +50,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/instrument');
 
+
 });
--- musicApp/www/js/controllers.js
+++ musicApp/www/js/controllers.js
@@ -1,5 +1,6 @@
 angular.module('starter.controllers', [])
 
+
 .controller('InstrumentCtrl', function($scope) {
   var s = Snap("#keyboard");
   Drawing.drawKeyboard(s, 30, 50, 250, 80, 13); //TODO: define and pass in pressCB and releaseCB
