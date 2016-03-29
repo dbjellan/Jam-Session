37dcc68268dfd1af0667a15f48cad86cc498e3fd
awetzel1 <awetzel1@macalester.edu>
2016-03-29 15:34:29 -0500

    Lots of nice UI, keyboard is missing though

--- musicApp/www/js/controllers.js
+++ musicApp/www/js/controllers.js
@@ -3,7 +3,9 @@ angular.module('starter.controllers', [])
 
 .controller('InstrumentCtrl', function($scope) {
   var s = Snap("#keyboard");
-  Drawing.drawKeyboard(s, 30, 50, 250, 80, 13); //TODO: define and pass in pressCB and releaseCB
+  Drawing.drawKeyboard(s, 30, 50, 125, 40, 13); //TODO: define and pass in pressCB and releaseCB
+  //NOTE: change (125,40) back to (250, 80) once the keyboard is showing if it fits?
+ //Consider finding a way to resize the keyboard relatively
 
 })
 
