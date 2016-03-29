35f2be57f26b455bb7602f5bf42226ad329089ef
awetzel1 <awetzel1@macalester.edu>
2016-03-29 16:13:59 -0500

    keyboard found, UI okay but no behavior

--- musicApp/www/js/controllers.js
+++ musicApp/www/js/controllers.js
@@ -3,9 +3,8 @@ angular.module('starter.controllers', [])
 
 .controller('InstrumentCtrl', function($scope) {
   var s = Snap("#keyboard");
-  Drawing.drawKeyboard(s, 30, 50, 125, 40, 13); //TODO: define and pass in pressCB and releaseCB
-  //NOTE: change (125,40) back to (250, 80) once the keyboard is showing if it fits?
- //Consider finding a way to resize the keyboard relatively
+  Drawing.drawKeyboard(s, 30, 50, 250, 80, 13); //TODO: define and pass in pressCB and releaseCB
+ //Consider finding a way to resize the keyboard relatively (screen percentage/cb versus pixels)
 
 })
 
